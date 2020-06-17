e93c9bf332282f14cd5d94bce342add6d7c223df
LiYing22 <297761278@qq.com>
2020-06-17 16:01:58 +0800

    Modified Font Page
    The first commin.
    Font pages include student page, coach page and admin page.
    Run in IDEA:
    1.npm i
    2.npm run dev

---
+++ studentCoursesSystem-master/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Coach from "@/components/coach/Coach.vue"
+import Students from "@/components/students/Students"
+import Enrollments from "@/components/enrollments/Enrollments"
+import Aboutme from "@/components/Aboutme"
+import Admin from "@/components/admin/Admin.vue"
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      // name: 'HelloWorld',
+      redirect:"/students"
+    },
+    {
+      path:"/students",
+      name:"students",
+      component:Students
+    },
+    {
+      path:"/coach",
+      name:"coach",
+      component:Coach
+    },
+    {
+      path:"/admin",
+      name:"admin",
+      component:Admin
+    },
+    {
+      path:"/enrollments",
+      name:"enrollments",
+      component:Enrollments
+    },
+    {
+      path:"/aboutme",
+      name:"aboutme",
+      component:Aboutme
+    }
+
+  ]
+})
