4573b7c7d987ba437a08db92e72f3f0b8d83c21c
ZBRaphael <1084859741@qq.com>
2020-06-28 13:24:05 +0800

    修改了登陆

--- StarTrace_teacher/app/src/main/java/com/example/teacherstartrace/ui/activity/LoginActivity.kt
+++ StarTrace_teacher/app/src/main/java/com/example/teacherstartrace/ui/activity/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import androidx.annotation.StringRes
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
@@ -20,12 +21,17 @@ import com.example.teacherstartrace.base.BaseActivity
 import com.example.teacherstartrace.ui.login.LoggedInUserView
 import com.example.teacherstartrace.ui.login.LoginViewModel
 import com.example.teacherstartrace.ui.login.LoginViewModelFactory
+import com.example.teacherstartrace.util.ThreadUtil
+import com.example.teacherstartrace.util.URLProviderUtils
+import okhttp3.*
 import org.jetbrains.anko.startActivity
+import java.io.IOException
+import java.util.concurrent.ConcurrentHashMap
 
 class LoginActivity : BaseActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
-
+    var sessionId = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,114 +41,120 @@ class LoginActivity : BaseActivity() {
         val password = findViewById<EditText>(R.id.password)
         val login = findViewById<Button>(R.id.login)
         val register = findViewById<Button>(R.id.btn_register)
-        val loading = findViewById<ProgressBar>(R.id.loading)
-        register.setOnClickListener{
-            val intent = Intent(this,
-                RegisterActivity::class.java)
+
+        register.setOnClickListener {
+            val intent = Intent(
+                this,
+                RegisterActivity::class.java
+            )
             startActivity(intent)
         }
-        loginViewModel = ViewModelProviders.of(this,
-            LoginViewModelFactory()
-        )
-                .get(LoginViewModel::class.java)
 
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
+        login.setOnClickListener {
 
-            // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
+            val path = URLProviderUtils.loginUrl()
+            val builder = FormBody.Builder()
+            builder.add("stuUsername", username.text.toString())
+            builder.add("stuPassword", password.text.toString())
+            val formBody = builder.build()
+            val cookieStore: ConcurrentHashMap<String, List<Cookie>> =
+                ConcurrentHashMap()
+            val mOkHttpClient = OkHttpClient.Builder()
+                .cookieJar(object : CookieJar {
+                    //这里可以做cookie传递，保存等操作
+                    override fun saveFromResponse(
+                        url: HttpUrl,
+                        cookies: List<Cookie>
+                    ) { //可以做保存cookies操作
+                        cookieStore.put(url.host, cookies)
+//                        println(cookies[0].hostOnly)
+                        val cookieStr = StringBuilder();
+                        cookieStr.append(cookies[0].name).append("=").append(cookies[0].value + ";");
+                        sessionId = cookieStr.toString()
+                        println("cookies:$cookies")
+                    }
 
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }
-        })
+                    override fun loadForRequest(url: HttpUrl): List<Cookie> { //加载新的cookies
+                        val cookies: List<Cookie>? = cookieStore.get(url.host)
+                        return cookies ?: ArrayList()
+                    }
+                })
+                .build()
+            Log.v("stu", formBody.toString())
 
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
+            val request = Request.Builder()
+                .url(path)
+                .post(formBody)
+                .build()
+            mOkHttpClient.newCall(request).enqueue(object : Callback {
+                /**
+                 * 子线程调用
+                 */
+                override fun onFailure(call: Call, e: IOException) {
 
-            loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
-            }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
-        })
-
-        username.afterTextChanged {
-            loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
-            )
-        }
-
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
-                        username.text.toString(),
-                        password.text.toString()
-                )
-            }
-
-            setOnEditorActionListener { _, actionId, _ ->
-                when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                                username.text.toString(),
-                                password.text.toString()
-                        )
+                    Log.v("http", "获取数据出错：" + path)
+                    throw(e)
                 }
-                false
-            }
 
-            login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
-            }
+                /**
+                 * 子线程调用
+                 */
+
+                override fun onResponse(call: Call, response: Response) {
+
+
+                    Log.v("http", "获取数据成功：" + Thread.currentThread().name)
+                    val result = response.body?.string().toString()
+
+                    println("result：$result")
+                    if (result == "success") {
+                        updateUiWithUser(username.text.toString())
+                    }
+                    else{
+
+                        Toast.makeText(
+                            applicationContext,
+                            "密码错误或用户不存在",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+
+                }
+
+            })
+
+
 
         }
-    }
 
+
+    }
     override fun getLayoutId(): Int {
         return R.layout.activity_login
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
-        Toast.makeText(
-                applicationContext,
-                "$welcome $displayName",
-                Toast.LENGTH_LONG
-        ).show()
-        val intent = Intent(this,
-            MainActivity::class.java)
+    private fun updateUiWithUser(username: String) {
+        ThreadUtil.runOnMainThread(object : Runnable {
+            override fun run() {
+                //tanchuang
+                val welcome = getString(R.string.welcome)
+                Toast.makeText(
+                    applicationContext,
+                    "$welcome $username",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+
+
+        val intent = Intent(
+            this,
+            MainActivity::class.java
+        )
+        intent.putExtra("username", username);
+        intent.putExtra("sessionId",sessionId);
         startActivity(intent)
     }
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
-    }
-}
-
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
 }
